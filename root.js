c8f44cdfb51b85dea2a8c1fced38dcaeaaf5bcb3
karam karim <BlackGhost3@users.noreply.github.com>
2017-07-19 06:19:06 +0300

    Update root.js

--- root.js
+++ root.js
@@ -1,5 +1,10 @@
 (()=>{
  let h = document.createElement("h1");
+ let p = document.createElement("p");
  h.textContent = "hey there, i'm still building this site";
  document.body.appendChild(h);
+ document.body.appendChild(p);
+ document.addEventListener("mousemove", e => {
+  p.textContent = `x: ${e.clientX}, y: ${e.clientY}`;
+ });
 })();
