fe52d9c96e89dde95ab02ffdb9299388e90acae4
karam karim <BlackGhost3@users.noreply.github.com>
2017-07-19 06:24:28 +0300

    Update root.js

--- root.js
+++ root.js
@@ -1,10 +1,13 @@
 (()=>{
  let h = document.createElement("h1");
  let p = document.createElement("p");
+ let bg = "#000000";
  h.textContent = "hey there, i'm still building this site";
  document.body.appendChild(h);
  document.body.appendChild(p);
  document.addEventListener("mousemove", e => {
+  bg = `#${e.clientX}${e.clientY}`;
   p.textContent = `x: ${e.clientX}, y: ${e.clientY}`;
+  document.body.style.backgroundColor = bg;
  });
 })();
