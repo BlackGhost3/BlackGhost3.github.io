ac94a52824c3cdb09967675bdfbbaa33d01486bb
karam karim <karam.karim.q@gmail.com>
2017-08-24 15:12:18 +0300

    hola

--- root.js
+++ root.js
@@ -23,7 +23,7 @@
   reverse_bg = bg.split("").reverse().join("");
   p.innerHTML = f;
   console.log("color:", bg, " - R:",reverse_bg);
-  p.style.color = reverse_bg;
+  console.log(p.style.color = reverse_bg);
   h.style.color = reverse_bg;
   // document.body.style.backgroundColor = bg;
  });
