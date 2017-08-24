b4c6a74c9ebf2b04883a864d410df7e68c609f14
karam karim <karam.karim.q@gmail.com>
2017-08-24 15:43:18 +0300

    hola

--- root.js
+++ root.js
@@ -23,6 +23,7 @@
   window.extra.bg = bg;
   reverse_bg = bg.slice(1, -1).split("").reverse().join("");
   reverse_bg = "#" + reverse_bg;
+  f += `<br/> font color: ${reverse_bg}`;
   p.innerHTML = f;
   p.style.color = reverse_bg;
   h.style.color = reverse_bg;
