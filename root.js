f300da1c8ce238e300c8d7fb9333e1193d8323bf
karam karim <karam.karim.q@gmail.com>
2017-08-24 16:50:13 +0300

    hola

--- root.js
+++ root.js
@@ -33,8 +33,8 @@
   p.style.color = reverse_bg;
   h.style.color = reverse_bg;
   document.body.style.backgroundColor = bg;
-  let TX = (x / 60),
-      TY = (y / 60);
+  let TX = (x / 10),
+      TY = (y / 10);
   parent.style = `transform: translate(${TX}px, ${TY}px);`;
 
  });
